7a0179f42171aaf8dac964c1a16baa2392b23232
sylas08 <lundonk2025@masseyhigh.school.nz>
2025-02-21 11:46:37 +1300

    name, age and payment method

---
+++ C_05_name_age_pay.py
@@ -0,0 +1,78 @@
+# Function goes here
+def int_check(question):
+    """Checks users enter an integer"""
+
+    error = "Oops - please enter an integer more than zero."
+
+    while True:
+
+        try:
+            # Change the response to an integer and check that it's more than zero
+            response = int(input(question))
+
+            return response
+
+        except ValueError:
+            print(error)
+
+
+def not_blank(question):
+    """Checks that a user response is not blank"""
+
+    while True:
+        response = input(question)
+
+        if response != "":
+            return response
+
+        print("Sorry, this can't be a blank. Please try again.\n")
+
+
+def string_check(question, valid_answers=('yes', 'no'),
+                 num_letters=1):
+    """Checks that users enter the full world
+        or the 'n' letter/s of a word from a list of valid responses"""
+
+    while True:
+
+        response = input(question).lower()
+
+        for item in valid_answers:
+
+            # check if the response is the entire world
+            if response == item:
+                return item
+
+            # check if it's the first letter
+            elif response == item[:num_letters]:
+                return item
+
+        print(f"Please choose an option from {valid_answers}")
+
+
+# Main Route goes here
+payment_ans = ('cash', 'credit')
+
+# loop for testing purposes...
+while True:
+    print()
+
+    # ask user for their name (and check it's not blank)
+    name = not_blank("Name: ")
+
+    # Ask for their age and check it's between 12 and 120
+    age = int_check("Age: ")
+
+    # Output error message / success message
+    if age < 12:
+        print(f"{name} is too young")
+        continue
+    elif age > 120:
+        print(f"{name} is too old")
+        continue
+    else:
+        pass
+
+    # ask user for payment method (cash / credit / ca / cr)
+    pay_method = string_check("Payment method: ", payment_ans, 2)
+    print(f"{name} has bought a ticket ({pay_method})")
